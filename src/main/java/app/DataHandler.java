519eff75e2e134b61c5667e54bdd39a1305e0ebf
Kateryna Livinska <kateryna.livinska@gmail.com>
2024-11-03 11:59:36 +0200

    Add DataHandler

---
+++ src/main/java/app/DataHandler.java
@@ -0,0 +1,22 @@
+package app;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.lang.Thread.currentThread;
+
+public class DataHandler {
+    String[] fruits = new DataRepository().getData();
+
+    public void getOutput() {
+        // критичний блок коду
+        synchronized (this) {
+            StringBuilder sb = new StringBuilder();
+            AtomicInteger count = new AtomicInteger(1);
+            for (String fruit : fruits) {
+                sb.append(String.format("(%d) %s ", count.intValue(), fruit));
+                count.getAndIncrement();
+            }
+            System.out.println(currentThread().getName() + ": " + sb);
+        }
+    }
+}
